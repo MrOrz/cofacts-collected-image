826c6a5738c85f2bf23f59cc23533889931f05b9
MrOrz <johnsonliang7@gmail.com>
2022-02-06 07:56:58 +0800

    Install material-ui & react-router

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import CssBaseline from '@mui/material/CssBaseline';
+import { BrowserRouter } from "react-router-dom";
+
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+    <CssBaseline />
   </React.StrictMode>,
   document.getElementById('root')
 );
