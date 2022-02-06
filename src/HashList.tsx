d1ce97856efab8b8809903c3f185c8705839d963
MrOrz <johnsonliang7@gmail.com>
2022-02-06 11:11:38 +0800

    Paginated HashList

--- src/HashList.tsx
+++ src/HashList.tsx
@@ -1,31 +1,59 @@
-import React, { useMemo } from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useMemo, useState } from 'react';
+import Pagination from '@mui/material/Pagination';
+
 import type { HashMap } from './scripts/genHash';
 
 type Props = {
   hashMap: HashMap;
 };
 
+const PAGE_SIZE = 100;
+const Formatter = new Intl.NumberFormat();
+
 const HashList: React.FC<Props> = ({hashMap}) => {
-  const { } = useParams();
+  const [page, setPage] = useState(1);
+
   const hashEntries = useMemo(() => {
     return Object.entries(hashMap).sort(([, fns1], [, fns2]) => fns2.length - fns1.length)
   }, [hashMap]);
 
-  return <ul className="">
-    {hashEntries.map(([hash, fileNames]) => (
-      <li key={hash}>
-        <code>{hash}</code> ({fileNames.length})
-        <ul>
-          {
-            fileNames.slice(0, 3).map(fn => (
-              <li key={fn}><img height="100" src={`/images/${fn}`} alt={fn} /></li>
-            ))
-          }
-        </ul>
-      </li>
-    ))}
-  </ul>
+  const handlePageChange = (e: unknown, value: number) => {
+    setPage(value);
+  }
+
+  const paginationElem =(
+    <Pagination
+      count={Math.ceil(hashEntries.length / PAGE_SIZE)}
+      page={page}
+      onChange={handlePageChange}
+    />
+  );
+
+  return <>
+    <p>
+      {Formatter.format(hashEntries.length)} hashes
+    </p>
+    {paginationElem}
+    <ul>
+      {
+        hashEntries
+          .slice((page-1) * PAGE_SIZE, page * PAGE_SIZE)
+          .map(([hash, fileNames]) => (
+            <li key={hash}>
+              <code>{hash}</code> ({fileNames.length})
+              <ul>
+                {
+                  fileNames.slice(0, 3).map(fn => (
+                    <li key={fn}><img height="100" src={`/images/${fn}`} alt={fn} /></li>
+                  ))
+                }
+              </ul>
+            </li>
+          ))
+      }
+    </ul>
+    {paginationElem}
+  </>
 }
 
 export default HashList;
